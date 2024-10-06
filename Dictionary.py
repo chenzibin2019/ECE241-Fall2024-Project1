59e37e1c3eb055ab796ef8dc5b758f3d71165d16
zibin <chenzibin2017@outlook.com>
2024-10-06 16:45:50 -0400

    Initial commit

---
+++ Dictionary.py
@@ -0,0 +1,68 @@
+"""
+ECE241 - Fall 2024
+Project 1 - Dictionary
+Fill in your code in this file!
+"""
+
+PUNC = "’!()-[]{};:’\"\,<>./?@#$%^&*_~’" # this is a constant that you might use in this project.
+
+class Dictionary:
+    def __init__(self):
+        self.words = []  # store all your words here
+
+    def insert(self, word, position):
+        """
+
+        :param word:
+        :param position:
+        :return:
+        """
+        pass
+
+    def shuffle(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def loadFromFile(self, filename):
+        """
+
+        :param filename:
+        :return:
+        """
+        pass
+
+
+    def linearSearch(self, word):
+        """
+
+        :param word:
+        :return:
+        """
+        pass
+
+    def binarySearch(self, word):
+        """
+
+        :param word:
+        :return:
+        """
+        pass
+
+    def insertionSort(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def spellCheck(self, sentence):
+        """
+
+        :param sentence:
+        :return:
+        """
+        pass
+
